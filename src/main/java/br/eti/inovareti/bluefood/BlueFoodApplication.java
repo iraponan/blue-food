c5d0c525af6eb50c662b02a18cbed38e2500d231
Iraponan Marinho <iraponan.aires@outlook.com>
2021-07-13 20:42:07 -0300

    Projeto Criado

---
+++ src/main/java/br/eti/inovareti/bluefood/BlueFoodApplication.java
@@ -0,0 +1,13 @@
+package br.eti.inovareti.bluefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlueFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/eti/inovareti/bluefood/BlueFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.inovareti.bluefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
